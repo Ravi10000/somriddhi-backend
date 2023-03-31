ba04c08cb4c43b3ce48bc9fedc6eafc1a63189da
Your Name~ <you@example.com>
2023-03-31 13:24:21 +0530

    changes done

--- controllers/ReadFile.js
+++ controllers/ReadFile.js
@@ -4,16 +4,18 @@ const fs = require('fs');
 
 exports.getExcelData = async (req, res) => {
     try {
-        let userId = req.user.id;
-        if (!userId) throw new Error("Not logged in or Session expired.");
+        // let userId = req.user.id;
+        // if (!userId) throw new Error("Not logged in or Session expired.");
 
         readexcelfile(fs.createReadStream('sample.xlsx')).then((rows) => {
             console.log(rows);
+            res.json({
+                "status": true,
+                "data": rows
+            });
         })
 
-        res.json({
-            "status": true
-        });
+
 
     } catch (error) {
         console.error(error.message);
