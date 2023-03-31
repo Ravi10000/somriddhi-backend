196f2e5f74a49a78ef9b8cc4a72733f5db295f46
ravi <pabhinav400@gmail.com>
2023-03-31 13:40:42 +0530

    merge

--- controllers/ReadFile.js
+++ controllers/ReadFile.js
@@ -1,22 +1,20 @@
-const readexcelfile = require('read-excel-file/node');
-const fs = require('fs');
-
+const readexcelfile = require("read-excel-file/node");
+const fs = require("fs");
 
 exports.getExcelData = async (req, res) => {
-    try {
-        let userId = req.user.id;
-        if (!userId) throw new Error("Not logged in or Session expired.");
+  try {
+    let userId = req?.user?.id;
+    if (!userId) throw new Error("Not logged in or Session expired.");
 
-        readexcelfile(fs.createReadStream('sample.xlsx')).then((rows) => {
-            console.log(rows);
-        })
+    readexcelfile(fs.createReadStream("sample.xlsx")).then((rows) => {
+      console.log(rows);
+    });
 
-        res.json({
-            "status": true
-        });
-
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-    }
-}
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+};
