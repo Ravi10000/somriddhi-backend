48a158eb0f1dc181e5d86636b362e42cd4bf3213
Your Name~ <you@example.com>
2023-03-31 16:35:06 +0530

    changes

--- controllers/ReadFile.js
+++ controllers/ReadFile.js
@@ -1,19 +1,56 @@
 const readexcelfile = require('read-excel-file/node');
-const fs = require('fs');
-
+let fs = require('fs');
+const reader = require('xlsx')
 
 exports.getExcelData = async (req, res) => {
     try {
+
+        const file = reader.readFile('sample.xlsx')
+
+        let data = []
+
+        const sheets = file.SheetNames
+
+        for (let i = 0; i < 1; i++) {
+            const temp = reader.utils.sheet_to_json(
+                file.Sheets[file.SheetNames[0]])
+            temp.forEach((res) => {
+                data.push(res)
+            })
+        }
+        let result = [];
+        for (let i = 1; i < data.length; i++) {
+            result.push(data[i]);
+        }
+
+        // Printing data
+        console.log(data)
+        res.json({
+            "status": true,
+            "data": result
+        });
+
+        // reader = fs.createReadStream('sample.xlsx');
+
+        // // Read and display the file data on console
+        // reader.on('data', function (chunk) {
+        //     // console.log(chunk);
+        //     res.json({
+        //         "status": true,
+        //         "data": chunk
+        //     });
+        // });
+
         // let userId = req.user.id;
         // if (!userId) throw new Error("Not logged in or Session expired.");
 
-        readexcelfile(fs.createReadStream('sample.xlsx')).then((rows) => {
-            console.log(rows);
-            res.json({
-                "status": true,
-                "data": rows
-            });
-        })
+        // readexcelfile(fs.createReadStream('sample.xlsx')).then((rows) => {
+        //     console.log(rows);
+        //     res.json({
+        //         "status": true,
+        //         "data": rows
+        //     });
+        // })
 
 
 
