d2c57d094b8de13f6c549dd00241ebc061e49472
Arpita Saxena <arpitasaxena555@gmail.com>
2023-04-03 14:29:52 +0530

    ssl

--- index.js
+++ index.js
@@ -4,32 +4,20 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
+const https = require('https');
+const http = require('http');
+var fs = require('fs');
 
 mongoose.set("strictQuery", false);
 mongoose.Promise = global.Promise;
 
 env.config();
 
-app.use(function (req, res, next) {
+var privateKey  = fs.readFileSync('api_somriddhi_store.key', 'utf8');
+var certificate = fs.readFileSync('api_somriddhi_store.crt', 'utf8');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://somriddhi.store');
+var credentials = {key: privateKey, cert: certificate};
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(cors());
 
 app.use(express.static("uploads"));
 app.use(express.json());
@@ -57,6 +45,23 @@ app.use("/api", require("./route/payment"));
 
 mongoose.connect(process.env.MONGO_URL);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT} `);
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is listening on port ${process.env.PORT} `);
+// });
+
+// https.createServer(options, function (req, res) {
+//   res.writeHead(200);
+//   res.end("Welcome to Node.js HTTPS Servern");
+// }).listen(8443);
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(process.env.HTTP_PORT,()=>{
+  console.log(`HTTP Server is listening on port ${process.env.HTTP_PORT} `);
+});
+
+httpsServer.listen(process.env.HTTPS_PORT,()=>{
+  console.log(`HTTPS Server is listening on port ${process.env.HTTPS_PORT} `);
 });
