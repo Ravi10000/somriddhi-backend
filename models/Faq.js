3fa8819c10907ac3102ab38f0f89588e073bd443
ravi <pabhinav400@gmail.com>
2023-05-15 12:56:59 +0530

    latest commit

--- models/Faq.js
+++ models/Faq.js
@@ -1,28 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const FaqSchema = new Schema({
-    question: {
-        type: String,
-        required: true
-    },
-    answer: {
-        type: String,
-        required: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        default: Date.now
-    }
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: Date.now,
+  },
 });
-const Faq = mongoose.model('faq', FaqSchema);
+const Faq = mongoose.model("faq", FaqSchema);
 module.exports = Faq;
-
-//create faq
-//update faq
-//get all faqs
-//delete faq
